2133f606515bfcfb2c2d9abc5a878ee8a866a710
t3chy <elam.day.friedland@gmail.com>
2020-07-31 10:54:53 -0700

    maybe this isn't the best system

--- api.py
+++ api.py
@@ -21,7 +21,7 @@ app = flask.Flask(__name__)
 @app.route('/move', methods=['GET','PUT'])
 def move():
     if request.method == "PUT":
-        dir = request.data["dir"]
+        dir = request.form["dir"]
         if dir == "forward":
             forward()
             return("going forward")
