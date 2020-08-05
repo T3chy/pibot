f33e94db0caedc86df769424155bbcfb5d0f70b3
Elam Day <elam.day.friedland@gmail.com>
2020-08-05 16:55:32 -0700

    cooler test sequence, it do be skidding though maybe get on that

--- testbot.py
+++ testbot.py
@@ -1,10 +1,11 @@
 import requests
 import time
-time.sleep(10)
-url = "http://192.168.3.127:5000/move"
+time.sleep(2)
+ip = "192.168.1.242"
+url = "http://" + ip + ":5000/move"
 
-pattern = [['backward','45'],['forward','45'],['backward','-45'],['forward','-45'],['stop','0']]
+pattern = [['stop','90',2],['forward','90',.5],['stop','-90',2],['forward','-90',.5], ['stop','45',2],['forward','45',1],['backward','45',1],['stop','-45',2],['forward','-45',1],['backward','-45',1],['stop','0',0]]
 for move in pattern:
     payload = { 'dir' :move[0], 'turndeg':move[1] }
     res= requests.put(url, data=payload)    
-    time.sleep(.5)
+    time.sleep(move[2])
